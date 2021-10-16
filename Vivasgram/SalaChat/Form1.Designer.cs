d9713c4155eae0ec0175534c7d2c87352cc006a4
Juanjo <juanjosegoberna.figueira@gmail.com>
2021-10-16 13:30:07 +0200

    Proyecto

---
+++ Vivasgram/SalaChat/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace SalaChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 52);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(406, 406);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(53, 509);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(406, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(616, 509);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 567);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Vivasgram";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Vivasgram/VivasGramm/Chat.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VivasGramm
+{
+    class Chat
+    {
+        private List<Socket> usuarios = new List<Socket>();
+        public Object l = new object();
+
+        public int ComprobarPuerto()
+        {
+            Console.WriteLine("entrar");
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:\\puerto.txt")) 
+                {
+                    string linea = sr.ReadLine();
+                    if (linea != null)
+                    {
+                        try
+                        {
+                            int numPuerto = Convert.ToInt32(linea);
+                            if (numPuerto > 10000 && numPuerto <= IPEndPoint.MaxPort)
+                            {
+                                Console.WriteLine("Valor válido");
+                                return numPuerto;
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Error");
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No hay archivo");
+            }
+            return 10000;
+        }
+
+        public void EnvioMensaje(string m, IPEndPoint ie)
+        {
+            IPEndPoint info;
+            lock (l)
+            {
+                for (int i = usuarios.Count; i > 0; i--)
+                {
+                    info = (IPEndPoint)usuarios[i - 1].RemoteEndPoint;
+                    using (NetworkStream ns = new NetworkStream(usuarios[i - 1]))
+                    using (StreamWriter sw = new StreamWriter(ns))
+                    {
+                        try
+                        {
+                            if (ie.Port != info.Port)
+                            {
+                                //Usuarios conectados en el momento -> usuarios.Count
+                                sw.WriteLine("Ip: {0}, Puerto:{1}: {2}", ie.Address, ie.Port, m);
+                                sw.Flush();
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "");
+                        }
+                    }
+                }
+            }
+        }
+
+        public void IniciarServer()
+        {
+            bool puertoCorrecto = false;
+            int puerto = ComprobarPuerto();
+            while (!puertoCorrecto)
+            {
+                try
+                {
+                    IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), puerto);
+                    puertoCorrecto = true;
+
+                    Socket socketConexion = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    socketConexion.Bind(endpoint);
+                    socketConexion.Listen(2);
+
+                    while (true)
+                    {
+                        Socket socketCliente = socketConexion.Accept();
+                        Thread hilos = new Thread(HiloCliente);
+                        hilos.Start(socketCliente);
+                    }
+                }
+                catch (SocketException e) when (e.ErrorCode == (int)SocketError.AddressAlreadyInUse)
+                {
+
+                    if (puerto < IPEndPoint.MaxPort)
+                    {
+                        puerto++;
+                    }
+                    else
+                    {
+                        puerto = 10000;
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+
+        public void HiloCliente(object Socket)
+        {
+            Socket socketusuario = (Socket)Socket;
+            string mensaje;
+            bool cerrarChat = false;
+
+            lock (l)
+            {
+                usuarios.Add(socketusuario);
+            }
+
+            IPEndPoint info = (IPEndPoint)socketusuario.RemoteEndPoint;
+
+            using (NetworkStream ns = new NetworkStream(socketusuario))
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns))
+            {
+                sw.WriteLine("Wellcome Puerto:{0} || IP:{1}", info.Port, info.Address);
+                lock (l)
+                {
+                    sw.WriteLine("Personas conectadas:{0} ", usuarios.Count);
+                }
+                sw.Flush();
+
+                while (!cerrarChat)
+                {
+                    try
+                    {
+                        mensaje = sr.ReadLine();
+                        if (mensaje != null)
+                        {
+                            if (mensaje == "salir")
+                            {
+                                cerrarChat = true;
+                                EnvioMensaje("Se ha desconectado", info);
+                            }
+                            else
+                            {
+                                EnvioMensaje(mensaje, info);
+                            }
+                        }
+
+                    }
+                    catch (IOException)
+                    {
+                        Console.WriteLine("Entro aqui, se marchó" + info.Port);
+                        socketusuario.Close();
+                        lock (l)
+                        {
+                            usuarios.Remove(socketusuario);
+                        }
+                        cerrarChat = true;
+                    }
+
+
+                }
+
+            }
+        }
+    }
+}
